08300fb22c9a2107e5e3e8fb5a764432f76d23e9
CharlieB50 <96846030+CharlieB50@users.noreply.github.com>
2022-01-03 15:29:59 -0600

    Update LoanDepo HomeWork.ps1

--- LoanDepo HomeWork.ps1
+++ LoanDepo HomeWork.ps1
@@ -1,1 +1,11 @@
-﻿Get-Recurr("abcdedcfd")
+Function Get-Recurr($inputArray) {    
+$arr_tmp = $inputArray.toCharArray()    
+$tmp = @{}    
+foreach ($arry in $arr_tmp){ 
+ if ($arry -in $tmp.Keys) {   
+     $arry            
+      break
+     } else {
+$tmp[$arry] = $null }
+    }
+}; Get-Recurr("abcdedcfd")
